35996742b8a2856e4104f050f7e3dc140b4aab5e
carlosandres00 <141078622+carlosandres00@users.noreply.github.com>
2023-08-02 22:45:11 +0000

    CLASE 2

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
-#primer archivo
+#primer python
+"""
 #ejercicio 1:
 print("hola mundo.")
+
+#ejercicio 2
+computador= 'hola mundo.'
+print(computador)
+
+#ejercicio 3
+name=input("¿Cual es tu nombre ")
+print (" Hola " + name + " es un placer! ")
+
+#ejercicio 4
+print (((3+2)/(2*5))**2)
+
+#ejercicio 5
+ht=float (input("escribe tus horas trabajadas "))
+ch=float(input("escribe el costo por hora "))
+valor= (ht*ch)
+print (valor)
+"""
+#ejercicio 6
+N=int(input("escribe un entero positivo "))
+print (N*(N+1)/2)
 
