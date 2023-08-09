89ceb1d8fe6255599cc9d23a22c1c4520380f9ac
carlosandres00 <141078622+carlosandres00@users.noreply.github.com>
2023-08-09 19:38:49 +0000

    biblio 10

--- main.py
+++ main.py
@@ -19,8 +19,35 @@ ht=float (input("escribe tus horas trabajadas "))
 ch=float(input("escribe el costo por hora "))
 valor= (ht*ch)
 print (valor)
-"""
+
 #ejercicio 6
 N=int(input("escribe un entero positivo "))
 print (N*(N+1)/2)
 
+#ejercicio 7 
+peso=float(input("introduzca su peso en KG aqui: "))
+estatura=float(input("introduzca su estatura en metros aqui: "))
+imc=round(peso/estatura**2,2)
+print("tu indice de masa corporal es ", imc)
+
+#ejercicio 8
+n= int(input("introduzca un numero entero: "))
+m= int(input("introduzca otro numero entero: "))
+d= n/m
+c= n//m
+r= n%m
+print("el resultado de la division es: ",d,", el residuo es :",r,",su cociente es:",c)
+
+#ejercicio 9
+inversion=float(input("cantidad a invertir: "))
+ia=float(input("el interes anual es: "))
+años=float(input("el numero de años invertido es: "))
+formula= inversion*(ia/100+1)**años
+print("el capital obtenido: ", formula)
+
+#ejercicio 10
+pp=int(input("cuantos payasos quieres pedir: "))
+pm=int(input("cuantas muñecas quieres pedir: "))
+formula= (pp*112)+(pm*75)
+print("el peso total del paquete es", formula,"g")
+"""
