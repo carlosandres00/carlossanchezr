8e9d775902d17a96b60628ed0a2c2a8ae2cb9639
carlosandres00 <141078622+carlosandres00@users.noreply.github.com>
2023-08-17 00:01:22 +0000

    python 3

--- main.py
+++ main.py
@@ -74,6 +74,7 @@ print("el costo final a pagar es: " + str(round(costo,2)) + "$")
 #segundo python
 
 #ejercicio 1
+
 print("introduce un numero entero: ")
 N=int(input())
 S=((N*(N+1)/2))
@@ -95,6 +96,7 @@ if C > 1: print("el divisor es menor que el dividendo")
 if C == 1: print("el divisor y el dividendo son iguales")
 
 #ejercicio 3
+
 print("ingrese la cantidad que desea invertir ")
 c=float(input())
 print("ingrese el interes anual ")
@@ -105,12 +107,106 @@ cap=(c*(i/100)**a)
 if cap<100000: print("la inversion de", c, "tiene baja rentabilidad")
 if cap>100000 and cap<1000000: print("la inversion de ", c , "tiene una rentabilidad moderada")
 if cap>1000000: ("la inversion de ", c, "es una buena inversion")
-"""
+
+#ejercicio 4
+print("numero de payasos vendidos")
+p=int(input())
+print("numero de muñecas vendidas")
+m=int(input())
+s="si"
+pg=p*112
+kg=s*75
+pesototal=pg+kg
+if pesototal>3000000:
+  print("desea enviarlo? ")
+  h=input()
+  if h==s: print("contenedor enviado")
+  else: print("contenedor no enviado")
+else: print("el peso total del pedido",pesototal)
+
+#tercer python
+
+#ejercicio 1
+
+def suma (a,b):
+  print(a+b)
+  return a+b
+print("escribe un numero")
+a=int(input())
+print("escribe otro numero ")
+b=int(input())
+suma(a,b)
+
+#ejercicio 2
+
+def resta (a,b):
+  print(a-b)
+  return a-b
+print("escribe un numero")
+a=int(input())
+print("escribe otro numero ")
+b=int(input())
+resta (a,b)
+
+#ejercicio 3
+
+def multiplicacion (a,b):
+  print(a*b)
+  return a*b
+print("escribe un numero")
+a=int(input())
+print("escribe otro numero ")
+b=int(input())
+multiplicacion (a,b)
+
 #ejercicio 4
 
+def division (a,b):
+  print(a/b)
+  return a/b
+print("escribe un numero")
+a=int(input())
+print("escribe otro numero ")
+b=int(input())
+division (a,b)
+"""
+#ejercicio 5
 
+def suma(a,b):
+  return a+b
 
+def resta(a,b):
+  return a-b
 
+def multiplicacion(a,b):
+  return a*b
 
+def division(a,b):
+  print(a/b)
+  if a ==0:
+     return a/b
+  else:
+     return("no es posible dividir por cero")
 
-    
+print("calculadora simple")
+print("operaciones simples")
+print("1. suma")
+print("2. resta")
+print("3. multiplicacion")
+print("4. division")
+
+opcion = input("seleccione una operacion (1/2/3/4): ")
+num1=float(input("ingrese el primer numero: "))
+num2=float(input("ingrese el segundo numero: "))
+
+if opcion =="1":
+  print("el resultado es: ", suma(num1, num2))
+elif opcion=="2":
+  print("el resultado es: ", resta(num1, num2))
+elif opcion=="3":
+  print("el resultado es: ", multiplicacion(num1, num2))
+elif opcion=="4":
+  print("el resultado es: ", division(num1, num2))
+else:
+  print("opcion invalida. Por favor ingrese una operacion valida.")
+  
